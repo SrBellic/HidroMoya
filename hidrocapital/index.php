71d0ae3394530d87693956d5cb29b2a2bd21d979
Bellic <159970540+SrBellic@users.noreply.github.com>
2024-03-14 12:13:50 -0400

    Update index.php
    Signed by: Bellic

--- hidrocapital/index.php
+++ hidrocapital/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta copyright="Bellic">
+	<meta name="copyright" content="Bellic">
 	<title>HidroMoya</title>
     <link rel="stylesheet" href="css/css/bootstrap.min.css">
     <!link rel="stylesheet" href="css/custom/custom.css">
